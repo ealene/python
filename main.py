030b5450df0eeda8f4129eae90c0030ed71427e8
ealene <2643397546@qq.com>
2021-05-18 12:12:21 +0800

    改进猜数游戏：使用random随机数

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 # python基础知识学习， 实践
 
+import random
 
-i=0
-while i<5:
+randnum = random.randint(1, 10)
+i = 0
+while i < 5:
     temp = input("pls input num:")
     num = int(temp)
-    if num == 3:
+    if num == randnum:
         print("yes")
         break
     else:
         print("no")
-        if num>3:
-            print("it's little than",num)
+        if num > randnum:
+            print("it's little than", num)
         else:
-            print("it's bigger than",num)
-    i=i+1
+            print("it's bigger than", num)
+    i = i + 1
 else:
     print("sorry no times !")
-
-
-
 
 print("End")
